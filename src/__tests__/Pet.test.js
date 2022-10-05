226b89140285e3e9783bd58e934683fcdb554741
joewallacerose <2565536r@student.gla.ac.uk>
2022-10-05 15:21:09 +0100

    Added carousel test

--- src/__tests__/Pet.test.js
+++ src/__tests__/Pet.test.js
@@ -21,3 +21,15 @@ test("Displays a default thumbnail", async () => {
   // If no photo info is given, check if we display 'none.jpg'
   expect(petThumbnail.src).toContain("none.jpg");
 });
+
+test("Displays a non-default thumbnail", async () => {
+  const pet = render(
+    <StaticRouter>
+      <Pet images={["1.jpg", "2.jpg", "3.jpg"]} />
+    </StaticRouter>
+  );
+
+  const petThumbnail = await pet.findByTestId("thumbnail");
+
+  expect(petThumbnail.src).toContain("1.jpg");
+});
